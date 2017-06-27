4c2d7d4c1bf66384f746b9356a6ddef4c415c7ef
Andrew Glines and Halle Tempest <andrew+halle@>
2017-06-27 11:16:36 -0700

    refactor front and back end

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,15 +1,10 @@
 var Weather = require('./../js/weather.js').weatherModule;
 
-var apiKey = "0a7afe3b0be03a26f34640932e9b921e";
-
-
-$(document).ready(function(){
+$(document).ready(function() {
+  var currentWeatherObject = new Weather();
   $('#weather-location').click(function() {
-    var city = $('#location').val();
-    $('#location').val("");
-    $('.showWeather').text("City is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      console.log(response);
+      var city = $('#location').val();
+      $('#location').val("");
+      currentWeatherObject.getWeather(city);
     });
   });
-});
